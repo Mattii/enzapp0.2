4af6b20c8c94363049b66558de51c446e4d46b54
Mateusz <sliwowski.mateusz@wp.pl>
2021-12-08 11:16:56 +0100

    pwa theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,8 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'pl'
+      lang: 'pl',
+      theme_color: "#A3D51C"
     }
   },
 
