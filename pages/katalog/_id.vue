e6f0cf2f4161498a0973e7d2c5e34d4427ec0dd1
Mateusz <sliwowski.mateusz@wp.pl>
2022-01-12 21:38:16 +0100

    crop detail page  tweeks

--- pages/katalog/_id.vue
+++ pages/katalog/_id.vue
@@ -20,20 +20,30 @@
           :src="require(`~/assets/img/${varietie[0].imgs[0]}`)"
           dark
         >
-          <div class="top-card-bar">
-            <div v-if="varietie[0].new" class="chip">NEW</div>
+          <div class="top-card-bar" dark>
+            <div v-if="varietie[0].new" class="chip glassmorphism">
+              <span>NEW</span>
+            </div>
+            <img
+              v-if="varietie[0].vitalis"
+              class="cardBadge glassmorphism"
+              src="/Vit_logo.png"
+            />
             <img
               v-if="varietie[0].provider == 'takii'"
-              class="takiiLogo"
+              class="cardBadge glassmorphism"
               src="/takiiLogoCut.png"
             />
           </div>
-          <v-card-title class="text-h4 text-uppercase"
+
+          <v-card-subtitle class="pb-0">{{
+            varietie[0].segment
+          }}</v-card-subtitle>
+          <v-card-title class="pt-0 text-h4 text-uppercase"
             >{{ varietie[0].name }}
             {{ varietie[0].code ? `(${varietie[0].code})` : "" }}
             {{ varietie[0].hybrid ? "F1" : "" }}</v-card-title
           >
-          <v-card-subtitle>{{ varietie[0].segment }}</v-card-subtitle>
         </v-img>
         <v-card-title class="">
           Osiagnij {{ varietie[0].purpose || "?" }}
@@ -58,70 +68,132 @@
           <div v-if="varietie[0].headShape" class="mb-4 text-subtitle-1">
             Kształt główki: {{ varietie[0].headShape || "?" }}
           </div>
-          <div v-if="varietie[0].vegetation" class="mb-4 text-subtitle-1">
-            Wegetacja: {{ varietie[0].vegetation.value }}
-            {{ varietie[0].vegetation.unit }}
-          </div>
-          <div v-if="varietie[0].harvest" class="mb-4 text-subtitle-1">
-            Zbiory:
-            <span v-for="(date, index) in varietie[0].harvest" :key="date">
-              {{
-                new Date(`${date}`).toLocaleDateString("pl-PL", {
-                  month: "long",
-                })
-              }}
-              <span v-if="index == '0' && varietie[0].harvest.length > 1"
-                >-</span
-              >
-            </span>
-          </div>
-          <div v-if="varietie[0].size" class="mb-4 text-subtitle-1">
-            Rozmiar: {{ varietie[0].size.value }} {{ varietie[0].size.unit }}
-          </div>
-          <div v-if="varietie[0].compaction" class="mb-4 text-subtitle-1">
-            Zagęszczenie: {{ varietie[0].compaction.value }}
-            {{ varietie[0].compaction.unit }}
-          </div>
-          <div class="mb-4 text-subtitle-1">
-            Podłoże:
-            <ul>
-              <li v-for="type in varietie[0].cultivation" :key="type">
-                {{ type }}
-              </li>
-            </ul>
-          </div>
-          <div v-if="varietie[0].resistance" class="mb-4 text-subtitle-1">
-            Odporności:
-            <ul>
-              <li v-for="type in varietie[0].resistance" :key="type">
-                {{ type }}
-              </li>
-            </ul>
-          </div>
-          <div>
-            <p>Cennik</p>
-            <ul>
-              <li v-for="(price, index) in varietiePrices" :key="index">
-                <p>
-                  <v-icon>mdi-package-variant</v-icon> {{ price.quantity }}
-                  {{ price.unit }}
-                  <v-icon v-if="price.caliber">mdi-diameter-outline</v-icon>
-                  {{ price.caliber ? price.caliber : "" }}
-                  {{ price.vitalis ? "BIO" : "" }}
-                  <br />
-                  <v-icon>mdi-cash-multiple</v-icon><strong>{{ Number(price.price).toFixed(2) }}</strong> ({{
-                    priceWithTax(price.price).toFixed(2)
-                  }}) zł
-                </p>
-              </li>
-            </ul>
-          </div>
-          <div class="mb-4 text-subtitle-1">
-            Opis:
-            <p>{{ varietie[0].description }}</p>
-          </div>
         </v-card-text>
       </v-card>
+      <div v-if="varietie[0].resistance">
+        <v-subheader class="mt-3">wegetacja...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div v-if="varietie[0].vegetation" class="text-subtitle-1">
+              {{ varietie[0].vegetation.value }}
+              {{ varietie[0].vegetation.unit }}
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div v-if="varietie[0].harvest">
+        <v-subheader class="mt-3">zbiory...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div class="text-subtitle-1">
+              <span v-for="(date, index) in varietie[0].harvest" :key="date">
+                {{
+                  new Date(`${date}`).toLocaleDateString("pl-PL", {
+                    month: "long",
+                  })
+                }}
+                <span v-if="index == '0' && varietie[0].harvest.length > 1"
+                  >-</span
+                >
+              </span>
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div v-if="varietie[0].size">
+        <v-subheader class="mt-3">rozmiar...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div v-if="varietie[0].size" class="text-subtitle-1">
+              {{ varietie[0].size.value }} {{ varietie[0].size.unit }}
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div v-if="varietie[0].compaction">
+        <v-subheader class="mt-3">zagęszczenie...</v-subheader>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header
+              class="text-subtitle-1"
+              disable-icon-rotate
+            >
+              {{ varietie[0].compaction.value }}
+              {{ varietie[0].compaction.unit }}
+              <template v-slot:actions>
+                <v-icon large color="primary"> mdi-calculator-variant </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Oblicz ile opakowań potrzebujesz
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
+      <div>
+        <v-subheader class="mt-3">podłoże...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div class="text-subtitle-1">
+              <ul>
+                <li v-for="type in varietie[0].cultivation" :key="type">
+                  {{ type }}
+                </li>
+              </ul>
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div v-if="varietie[0].resistance">
+        <v-subheader class="mt-3">odporności...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div class="text-subtitle-1">
+              <ul>
+                <li v-for="(type, index) in varietie[0].resistance" :key="type">
+                  {{ type }} <v-icon v-if="!index%2">mdi-bacteria</v-icon>
+                   <v-icon v-if="!!index%2">mdi-virus</v-icon>
+                </li>
+              </ul>
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div>
+        <v-subheader class="mt-3">cennik...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div>
+              <ul>
+                <li v-for="(price, index) in varietiePrices" :key="index">
+                  <p>
+                    <v-icon>mdi-package-variant</v-icon> {{ price.quantity }}
+                    {{ price.unit }}
+                    <v-icon v-if="price.caliber">mdi-diameter-outline</v-icon>
+                    {{ price.caliber ? price.caliber : "" }}
+                    {{ price.vitalis ? "BIO" : "" }}
+                    <br />
+                    <v-icon>mdi-cash-multiple</v-icon
+                    ><strong>{{ Number(price.price).toFixed(2) }}</strong> ({{
+                      priceWithTax(price.price).toFixed(2)
+                    }}) zł
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+      <div>
+        <v-subheader class="mt-3">opis...</v-subheader>
+        <v-card>
+          <v-card-text>
+            <div class="mb-4 text-subtitle-1">
+              <p>{{ varietie[0].description }}</p>
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
       <div>
         <v-subheader class="mt-4">polecamy również...</v-subheader>
         <base-medium-card
@@ -154,7 +226,8 @@ export default {
       (e) => e.id == this.$route.params.id
     );
     this.varietiesSegment = this.$store.getters.getVarieties.filter(
-      (e) => e.segment == this.varietie[0].segment && e.id !== this.$route.params.id
+      (e) =>
+        e.segment == this.varietie[0].segment && e.id !== this.$route.params.id
     );
     this.varietiePrices = this.$store.getters.getCropsPrice.filter((ele) => {
       //console.log(ele.name.toLowerCase(), this.varietie[0].name.toLowerCase());
@@ -171,3 +244,37 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.cardBadge {
+  width: 3rem;
+  height: 3rem;
+  padding: 0.2rem;
+}
+.top-card-bar {
+  display: flex;
+  width: 100%;
+  top: 0;
+  position: absolute;
+  padding: 1rem;
+  justify-content: space-between;
+  align-items: center;
+}
+.glassmorphism {
+  background: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(9px);
+  -webkit-backdrop-filter: blur(9px);
+  border-radius: 0.3rem;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+.chip {
+  font-weight: 800;
+  padding: 0.2rem;
+  height: 3rem;
+  width: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+</style>
